df0ec32ecd589d684c3a8dc062e1dfffe58ffc96
maoo <somalijs5@gmail.com>
2026-03-17 07:26:48 +0300

    cleanrs

--- backend/src/Managers/website/properties/delete.ts
+++ backend/src/Managers/website/properties/delete.ts
@@ -2,6 +2,7 @@ import { ClientSession } from "mongoose";
 import { ExpressRequest } from "../../../types/Express.js";
 import getPropertyModel from "../../../models/Properties.js";
 import { propertyDeleteSchema } from "./schema.js";
+import { deleteImageByUrl } from "../../../services/Files/upload/UploadFile.js";
 
 type props = {
   req: ExpressRequest;
@@ -15,10 +16,17 @@ const deleteProperty = async ({ req, session }: props) => {
   const property = await Property.findOne({ _id: id }).session(session);
   if (!property) throw new Error("Property not found");
   if (property.isDeleted) throw new Error("Property already deleted");
+  const allImages = [];
+  if (property.media?.cover) allImages.push(property.media.cover);
+  if (property.media?.images) allImages.push(...property.media.images);
 
   property.isDeleted = true;
   await property.save({ session });
-
+  if (allImages.length > 0) {
+    Promise.all(allImages.map((image) => deleteImageByUrl(image))).catch(() => {
+      console.log("Image cleanup failed (non-blocking)");
+    });
+  }
   return { message: "Property deleted successfully" };
 };
 
